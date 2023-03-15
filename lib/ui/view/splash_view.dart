11fbd4bba1ab9aca7a620badb0ab91800832ae21
Seyfettin Yıldırım <seyfi1082@gmail.com>
2023-03-15 10:17:32 +0300

    Create SplashView

---
+++ lib/ui/view/splash_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SplashView extends ConsumerWidget {
+  const SplashView({super.key});
+
+  static const routeName = '/splashView';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      child: Text(routeName),
+    );
+  }
+}
