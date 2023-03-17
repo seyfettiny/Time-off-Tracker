24b80ce5096031e29eccd86a445ab1cf69f47611
Seyfettin Yıldırım <seyfi1082@gmail.com>
2023-03-17 10:07:16 +0300

    Implement UserModel

---
+++ lib/app/enums/user_type.dart
@@ -0,0 +1,4 @@
+enum UserType {
+  Manager,
+  Employee,
+}
--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,7 +1,19 @@
-// class User {
-//   final String uid;
-//   final String? name;
-//   final String? email;
-//   final String? photoURL;
+import 'package:timeofftracker/app/enums/user_type.dart';
 
-// }
+class User {
+  final String uid;
+  final String name;
+  final String email;
+  final String? photoURL;
+  final int? timeOffBalance;
+  final UserType userType;
+
+  User({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.userType,
+    this.photoURL,
+    this.timeOffBalance = 28,
+  });
+}
