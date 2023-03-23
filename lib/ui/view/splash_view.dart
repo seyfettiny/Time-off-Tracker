e8e7f58cbe4b17f11bbd5cb16aad63edc7425ab5
Seyfettin Yıldırım <334321@ogr.ktu.edu.tr>
2023-03-23 10:15:16 +0300

    Add loading state widget for SplashView

--- lib/ui/view/splash_view.dart
+++ lib/ui/view/splash_view.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SplashView extends ConsumerWidget {
+class SplashView extends ConsumerStatefulWidget {
   const SplashView({super.key});
 
   static const routeName = '/splashView';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Text(routeName);
+  ConsumerState<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends ConsumerState<SplashView> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo_icon.png',
+              scale: 3,
+            ),
+            Image.asset(
+              'assets/images/logo_pepteam.png',
+              scale: 3,
+            ),
+            const SizedBox(height: 30),
+            const CircularProgressIndicator()
+          ],
+        ),
+      ),
+    );
   }
 }
